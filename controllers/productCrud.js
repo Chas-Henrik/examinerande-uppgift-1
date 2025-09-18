df229ce6f85304d355cc0f8cae5370b5fde98fa8
Kristoffer <kristoffer.benckert@chasacademy.se>
2025-09-18 14:47:13 +0200

    fixes getManufacturers

--- controllers/productCrud.js
+++ controllers/productCrud.js
@@ -1,22 +1,23 @@
+import { Manufacturer } from "../models/manufacturer.js"
 import { Product } from "../models/product.js"
-import merge from 'lodash/merge.js';
+import merge from "lodash/merge.js"
 
 // *** Additional operations ***
 
 export const getTotalStockValue = async () => {
-	const [totals] = await Product.aggregate([
-		{
-			$group: {
-				_id: null,
-				totalStockValue: { $sum: { $multiply: ["$price", "$amountInStock"] } }
-			}
-		}
-	]);
-	return totals.totalStockValue.toFixed(2);
-};
+  const [totals] = await Product.aggregate([
+    {
+      $group: {
+        _id: null,
+        totalStockValue: { $sum: { $multiply: ["$price", "$amountInStock"] } },
+      },
+    },
+  ])
+  return totals.totalStockValue.toFixed(2)
+}
 
 export const getTotalStockValueByManufacturer = async () => {
-	const totals = await Product.aggregate([
+  const totals = await Product.aggregate([
     {
       $lookup: {
         from: "manufacturers",
@@ -35,78 +36,94 @@ export const getTotalStockValueByManufacturer = async () => {
       },
     },
   ])
-	totals.forEach(item => item.totalStockValue = parseFloat(item.totalStockValue.toFixed(2)));
-	return totals;
-};
+  totals.forEach(
+    (item) =>
+      (item.totalStockValue = parseFloat(item.totalStockValue.toFixed(2)))
+  )
+  return totals
+}
 
 export const getLowStockProducts = async (threshold = 10) => {
-	return Product.find({ amountInStock: { $lt: threshold } });
-};
+  return Product.find({ amountInStock: { $lt: threshold } })
+}
 
 export const getCriticalStockProducts = async (threshold = 5) => {
-	return Product.find({ amountInStock: { $lt: threshold } }).select("name sku amountInStock manufacturer.name manufacturer.contact.name manufacturer.contact.phone manufacturer.contact.email");
-};
+  return Product.find({ amountInStock: { $lt: threshold } }).select(
+    "name sku amountInStock manufacturer.name manufacturer.contact.name manufacturer.contact.phone manufacturer.contact.email"
+  )
+}
 
 export const getManufacturers = async () => {
-	return Product.distinct("manufacturer.name");
-};
+  return Manufacturer.distinct("name")
+}
 
 // *** CRUD operations ***
 
 export const createProduct = async (productData) => {
-	return Product.create(productData);
-};
+  return Product.create(productData)
+}
 
 export const findProducts = async () => {
-	return Product.find();
-};
+  return Product.find()
+}
 
-export const findProductsWithFilterAndPagination = async (category, manufacturer, amountInStock, limit, page) => {
-	return await Product.aggregate([
-	// 1. Filter Stage
-	{
-		$match: {
-		...(category && { category: { $regex: category, $options: "i" } }),
-		...(manufacturer && { "manufacturer.name": { $regex: manufacturer, $options: "i" } }),
-		...(amountInStock && { amountInStock: { $lte: amountInStock } })
-		}
-	},
+export const findProductsWithFilterAndPagination = async (
+  category,
+  manufacturer,
+  amountInStock,
+  limit,
+  page
+) => {
+  return await Product.aggregate([
+    // 1. Filter Stage
+    {
+      $match: {
+        ...(category && { category: { $regex: category, $options: "i" } }),
+        ...(manufacturer && {
+          "manufacturer.name": { $regex: manufacturer, $options: "i" },
+        }),
+        ...(amountInStock && { amountInStock: { $lte: amountInStock } }),
+      },
+    },
 
-	// 2. Skip Stage (for pagination)
-	{ $skip: (page - 1) * limit },
+    // 2. Skip Stage (for pagination)
+    { $skip: (page - 1) * limit },
 
-	// 3. Limit Stage
-	{ $limit: limit }
-	])
-};
+    // 3. Limit Stage
+    { $limit: limit },
+  ])
+}
 
 export const findProduct = async (id) => {
-	return Product.findById(id);
-};
+  return Product.findById(id)
+}
 
 export const updateProduct = async (id, productData) => {
-	return Product.replaceOne({ _id: id }, productData, {
-		new: true,
-		runValidators: true,
-		upsert: false  // Do not create a new document if it doesn't exist
-	});
-};
+  return Product.replaceOne({ _id: id }, productData, {
+    new: true,
+    runValidators: true,
+    upsert: false, // Do not create a new document if it doesn't exist
+  })
+}
 
 export const patchProduct = async (id, productData) => {
-	
-	const existing = await Product.findById(id);
-	if (!existing) return null;
+  const existing = await Product.findById(id)
+  if (!existing) return null
 
-	// Deep merge the existing product with the patch input
-	const mergedData = merge({}, existing.toObject(), productData);
+  // Deep merge the existing product with the patch input
+  const mergedData = merge({}, existing.toObject(), productData)
 
-	return Product.findByIdAndUpdate(id, { $set: mergedData }, {
-		new: true,
-		runValidators: true,
-        upsert: false  // Do not create a new document if it doesn't exist
-	});
-};
+  return Product.findByIdAndUpdate(
+    id,
+    { $set: mergedData },
+    {
+      new: true,
+      runValidators: true,
+      upsert: false, // Do not create a new document if it doesn't exist
+    }
+  )
+}
 
 export const deleteProduct = async (id) => {
-    return Product.findByIdAndDelete(id);
-};
+  return Product.findByIdAndDelete(id)
+}
